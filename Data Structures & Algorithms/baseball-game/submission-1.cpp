c1a956e7dbc0e60ad4155d8a0a674c2e90607288
Varukolu Vishal <164896616+Vishal45718@users.noreply.github.com>
2026-08-20 12:56:04 +0530

    Add: baseball-game - submission-1

---
+++ Data Structures & Algorithms/baseball-game/submission-1.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    int calPoints(vector<string>& operations) {
+        
+        stack<int> st;
+
+        for(int i=0; i<operations.size(); i++)
+        {
+            if(operations[i] == "+")
+{
+    int a = st.top();
+    st.pop();
+
+    int b = st.top();
+    st.push(a);
+    st.push(a + b);
+}
+            else if(operations[i] == "D")
+            {
+                int a = st.top();
+                st.push(a*2);
+            }
+            else if(operations[i] == "C")
+            {
+                st.pop();
+            }
+            else
+            {
+                st.push(stoi(operations[i]));
+            }
+            
+        }
+
+        int ans = 0;
+
+        while(!st.empty())
+        {
+            int a = st.top();
+            st.pop();
+            ans += a;
+        }
+        return ans;
+    }
+};
