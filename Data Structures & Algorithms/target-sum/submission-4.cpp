0a409222dc5506d67e40f58830aef62f8739fc64
Varukolu Vishal <164896616+Vishal45718@users.noreply.github.com>
2026-08-24 16:55:31 +0530

    Add: target-sum - submission-4

---
+++ Data Structures & Algorithms/target-sum/submission-4.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int findTargetSumWays(vector<int>& nums, int target) {
+
+        int total = 0;
+
+        for (int num : nums) {
+            total += num;
+        }
+
+        if (abs(target) > total) {
+            return 0;
+        }
+
+        int P = target + total;
+
+        if (P % 2 != 0) {
+            return 0;
+        }
+
+        P /= 2;
+
+        vector<int> dp(P + 1, 0);
+        dp[0] = 1;
+
+        for (int num : nums) {
+            for (int sum = P; sum >= num; sum--) {
+                dp[sum] += dp[sum - num];
+            }
+        }
+
+        return dp[P];
+    }
+};
